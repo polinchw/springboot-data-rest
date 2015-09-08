96e730b1d764d001e9040c9af9bc8424124de048
polinchakb <polinchakb@elnk.com>
2015-09-08 10:41:07 -0400

    Printing out people in the database with a timer.

--- src/main/java/com/bpe/springboot/data/rest/timer/ScheduledTasks.java
+++ src/main/java/com/bpe/springboot/data/rest/timer/ScheduledTasks.java
@@ -2,10 +2,15 @@ package com.bpe.springboot.data.rest.timer;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Iterator;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.bpe.springboot.data.rest.entity.Person;
+import com.bpe.springboot.data.rest.repository.PersonRepository;
 
 @Component
 public class ScheduledTasks {
@@ -13,9 +18,19 @@ public class ScheduledTasks {
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
     
     private final Logger logger = Logger.getLogger(ScheduledTasks.class.getName());
+    
+    @Autowired
+    PersonRepository dao;
 
     @Scheduled(fixedRate = 5000)
-    public void reportCurrentTime() {       
+    public void printPeople() {       
         logger.info("The time is now " + dateFormat.format(new Date()));
+        Iterable<Person> people = dao.findAll();
+        Iterator<Person> peopleIt = people.iterator();
+        while(peopleIt.hasNext()) {
+        	Person p = peopleIt.next();
+            logger.info("Person inventory: "+p.getLastName()+", "+p.getFirstName());
+        }
+        
     }
 }
