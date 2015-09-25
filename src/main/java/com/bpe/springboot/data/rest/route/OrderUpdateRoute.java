af8ba3280a50caab3e62426604f1fa69867316f6
polinchakb <polinchakb@linux-mint>
2015-09-25 10:32:49 -0400

    Added file rename processor.

--- src/main/java/com/bpe/springboot/data/rest/route/OrderUpdateRoute.java
+++ src/main/java/com/bpe/springboot/data/rest/route/OrderUpdateRoute.java
@@ -3,6 +3,8 @@ package com.bpe.springboot.data.rest.route;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
+import com.bpe.springboot.data.rest.processor.FileRenameProcessor;
+
 /**
  * Camel route for processing incoming order update files.
  * 
@@ -16,7 +18,7 @@ public class OrderUpdateRoute extends RouteBuilder {
 	public void configure() throws Exception {		
 		from("file://{{order-update-inbox}}?delete=true")
 		  .convertBodyTo(byte[].class, "iso-8859-1")
-		  .to("bean:orderProcessor?method=updateOrder")
+		  .to("bean:orderProcessor?method=updateOrder").process(new FileRenameProcessor())
 		  .to("file://{{order-update-inbox-processed}}");
 	}
 
