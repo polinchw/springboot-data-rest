91191911d0361c64e020610dd10f7d96c9b1ae51
polinchakb <polinchakb@elnk.com>
2017-02-26 13:49:38 -0500

    Added Hero to test Angular2.

---
+++ src/main/java/com/bpe/springboot/data/rest/repository/HeroRepository.java
@@ -0,0 +1,13 @@
+package com.bpe.springboot.data.rest.repository;
+
+import com.bpe.springboot.data.rest.entity.Hero;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+/**
+ * Created by polinchakb on 2/26/17.
+ */
+@RepositoryRestResource(collectionResourceRel = "heros", path = "heros")
+public interface HeroRepository extends CrudRepository<Hero, Long> {
+}
